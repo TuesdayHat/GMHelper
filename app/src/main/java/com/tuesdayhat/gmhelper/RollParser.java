a705ef3909b9a620883317353bf0259f2a239b37
Student <example@example.com>
2018-04-10 14:03:03 -0700

    rollparser bloody well *should* now be able to detect numbers with multiple digits

--- app/src/main/java/com/tuesdayhat/gmhelper/RollParser.java
+++ app/src/main/java/com/tuesdayhat/gmhelper/RollParser.java
@@ -1,6 +1,36 @@
 package com.tuesdayhat.gmhelper;
 
 
+import java.util.ArrayList;
+import java.util.regex.*;
+
 public abstract class RollParser extends DiceRoller {
+    public ArrayList<String> parse (String input){
+        ArrayList<String> result = new ArrayList<>();
+
+        String[] words = input.split("");
+        for(int i =0;i < words.length;i++){
+            String word = words[i];
+            System.out.println(i + " -- " + word);
+        }
+
+        String numsRegex = "[0-9]";
+        for(int i =0; i<words.length;i++){
+            String cha = words[i];
+            int goTo = 0;
+            if(cha.matches(numsRegex)){
+                String readRolls = "";
+                for(int j = i; words[j].matches(numsRegex) && words[j+1].equals("d");j++){
+                    readRolls += words[j];
+                    goTo = j;
+                }
+                i = goTo;
+            }
+
+        }
+
+
+        return result;
+    }
 
 }
--- app/src/test/java/com/tuesdayhat/gmhelper/RollParserTest.java
+++ app/src/test/java/com/tuesdayhat/gmhelper/RollParserTest.java
@@ -2,10 +2,21 @@ package com.tuesdayhat.gmhelper;
 
 import org.junit.Test;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
 public class RollParserTest extends RollParser {
+    @Test
+    public void rawOutput() throws Exception{
+        System.out.println(parse("4d6"));
+    }
+
+    @Test
+    public void RollParser_parse_parseDiceRoll()throws Exception{
+        ArrayList<String> result = parse("4d6");
+        assert(Integer.parseInt(result.get(0)) >= 4 && Integer.parseInt(result.get(0)) <= 24);
+    }
 
 }
