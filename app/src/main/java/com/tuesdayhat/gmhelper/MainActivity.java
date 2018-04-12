202a16bd9390508cc458684ff4e58a5797b0b877
Student <example@example.com>
2018-04-12 11:02:58 -0700

    add MVP layout for simple dice roller

--- app/src/main/java/com/tuesdayhat/gmhelper/MainActivity.java
+++ app/src/main/java/com/tuesdayhat/gmhelper/MainActivity.java
@@ -2,8 +2,13 @@ package com.tuesdayhat.gmhelper;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+
+import butterknife.BindView;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.rollsInput) EditText
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
